fd37c47494593a34379b33b1f157153e5e21109b
Vishal-8155 <bagsariyaa@gmail.com>
2023-11-06 14:01:55 +0530

    Add

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -29,7 +29,7 @@ function Auth() {
       <CssBaseline />
       <Box
         sx={{
-          marginTop: 8,
+          marginTop: 9,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
